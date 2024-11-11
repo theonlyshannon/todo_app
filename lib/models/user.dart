b2167714cf07c4ff2272bd6dc5ef805d11faf547
theonlyshannon <sofwannuhaalfaruq@gmail.com>
2024-11-11 16:33:56 +0700

    First Commit

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+import 'package:hive/hive.dart';
+
+part 'user.g.dart';
+
+@HiveType(typeId: 0)
+class User extends HiveObject {
+  @HiveField(0)
+  String username;
+
+  @HiveField(1)
+  String password;
+
+  User(this.username, this.password);
+}
